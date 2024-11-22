fc772cb0bd3a2dbd57a1865f45411573c19dc1a1
Kyle Long <kylel95@vt.edu>
2024-11-22 00:45:48 -0500

    modify settings.py

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # Application definition
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "stripe/static")]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -50,6 +50,8 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "stripe/static")]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -137,12 +139,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
