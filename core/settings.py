c4fec358abbacd70539e5c30c8c135a29488430c
Kyle Long <kylel95@vt.edu>
2024-11-24 23:11:40 -0500

    add hosts back

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['https://ldgr-dashboard-react.onrender.com/']
+ALLOWED_HOSTS = ['https://ldgr-dashboard-react.onrender.com/', 'localhost']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
