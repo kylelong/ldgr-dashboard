aad92b7fa5f7a003307d928278c12e1592648d0d
Kyle Long <kylel95@vt.edu>
2024-11-27 18:20:21 -0500

    tailwind fix

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './dashboard/index.html'],
   theme: { extend: {} },
   plugins: [],
+  purge: false,
 }
